5e323ce585567c8b307b12dc27cfa69c5000983c
DheerajRay-01 <dheerajray2003@gmail.com>
2025-03-06 15:02:51 +0530

    fix

--- src/app.js
+++ src/app.js
@@ -12,20 +12,6 @@ app.use(cors({
     credentials: true
 }));
 
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", process.env.CORS_ORIGIN ); // Ensure this is set correctly
-  res.header("Access-Control-Allow-Credentials", "true");
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-
-  // Handle preflight requests
-  if (req.method === "OPTIONS") {
-    return res.sendStatus(204); // No content, just approve preflight
-  }
-
-  next();
-});
-
 
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
